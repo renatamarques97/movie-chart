26c0f5adc6ee130e295fdb24c190d3477b6aedb4
Renata Marques <renata.nrmarques@gmail.com>
2020-03-26 15:20:54 -0300

    initial structure and basic configuration

---
+++ db/migrate/20200326171542__create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.integer :watch_count
+      add_reference :genres, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
