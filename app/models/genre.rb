5d095475900c96b1117f2c81652a25f09586f2f8
Renata Marques <renata.nrmarques@gmail.com>
2020-03-26 15:27:55 -0300

    movie genres added

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,25 @@
 class Genre < ApplicationRecord
   has_many :movies
+  extend Enumerize
+
+  enumerize :title, in: { horror: 0,
+                          drama: 1,
+                          musical: 2,
+                          biography: 3,
+                          fantasy: 4,
+                          sport: 5,
+                          western: 6,
+                          mystery: 7,
+                          thriller: 8,
+                          war: 9,
+                          action: 10,
+                          comedy: 11,
+                          scifi: 12,
+                          romance: 13,
+                          history: 14,
+                          documentary: 15,
+                          animation: 16,
+                          adventure: 17,
+                          crime: 18
+                        }
 end
