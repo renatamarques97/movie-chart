30723d6f89a969b87ebb173f4e81f7c6b310fd37
Renata Marques <renata.nrmarques@gmail.com>
2020-03-27 20:22:30 -0300

    genre query updated

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,28 +1,21 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
-  # GET /movies
-  # GET /movies.json
   def index
     @movies = Movie.all
+    @data = Movie.joins(:genre).group("genres.title").count
   end
 
-  # GET /movies/1
-  # GET /movies/1.json
   def show
   end
 
-  # GET /movies/new
   def new
     @movie = Movie.new
   end
 
-  # GET /movies/1/edit
   def edit
   end
 
-  # POST /movies
-  # POST /movies.json
   def create
     @movie = Movie.new(movie_params)
 
@@ -37,8 +30,6 @@ class MoviesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /movies/1
-  # PATCH/PUT /movies/1.json
   def update
     respond_to do |format|
       if @movie.update(movie_params)
@@ -51,8 +42,6 @@ class MoviesController < ApplicationController
     end
   end
 
-  # DELETE /movies/1
-  # DELETE /movies/1.json
   def destroy
     @movie.destroy
     respond_to do |format|
@@ -62,12 +51,10 @@ class MoviesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_movie
       @movie = Movie.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def movie_params
       params.require(:movie).permit(:name, :genre_id, :watch_count)
     end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,25 +1,4 @@
 class Genre < ApplicationRecord
   extend Enumerize
   has_many :movies, dependent: :destroy
-
-  enumerize :title, in: { horror: 0,
-                          drama: 1,
-                          musical: 2,
-                          biography: 3,
-                          fantasy: 4,
-                          sport: 5,
-                          western: 6,
-                          mystery: 7,
-                          thriller: 8,
-                          war: 9,
-                          action: 10,
-                          comedy: 11,
-                          scifi: 12,
-                          romance: 13,
-                          history: 14,
-                          documentary: 15,
-                          animation: 16,
-                          adventure: 17,
-                          crime: 18
-                        }
 end
